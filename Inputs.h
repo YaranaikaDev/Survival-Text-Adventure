d460c42d172443829d189895e6539b3816961bfd
King Boo <thirteenthraven@gmail.com>
2014-08-18 02:51:58 -0400

    Modular Basics
    old code reformatted to be easier to manage

---
+++ Inputs.h
@@ -0,0 +1,15 @@
+#ifndef GUARD_Inputs
+#define GUARD_Inputs
+
+#include<iostream>
+#include<string>
+
+void menuCommand(std::string& termination_oper);
+
+class inputVars
+{
+public:
+	std::string selection_oper;		// Stores user input in main menu and event selection choices
+};
+
+#endif
